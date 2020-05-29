2ee0c0012192240a3b066c8d25ab056d840f065a
Jason <jason.brugger@gmail.com>
2020-05-29 12:34:14 -0500

    fixed memory issue

--- marquee.ino
+++ marquee.ino
@@ -13,22 +13,26 @@
 #define OFF (0x0)
 
 
-#define LETTER_G 0
-#define LETTER_A 1
-#define LETTER_M 2
-#define LETTER_E 3
-#define LETTER_O 4
-#define LETTER_V 5
-#define LETTER_R 6
-#define LETTER_SPC 7
-#define LETTER_J 8
-#define LETTER_S 9
-#define LETTER_N 10
-#define LETTER_K 11
-#define LETTER_T 12
-#define LETTER_H 13
-#define LETTER_Y 14
-#define LETTER_HRT 15
+#define CHR_G 0
+#define CHR_A 1
+#define CHR_M 2
+#define CHR_E 3
+#define CHR_O 4
+#define CHR_V 5
+#define CHR_R 6
+#define CHR_SPC 7
+#define CHR_J 8
+#define CHR_S 9
+#define CHR_N 10
+#define CHR_K 11
+#define CHR_T 12
+#define CHR_H 13
+#define CHR_Y 14
+#define CHR_HRT 15
+#define CHR_D 16
+#define CHR_I 17
+#define CHR_C 18
+#define CHR_SML 19
 
 
 void set_register(byte address, byte value)
@@ -90,22 +94,32 @@ uint8_t letter[][8] = {
   {102,102,102,126,102,102,102,0}, //H
   {102,102,102,60,24,24,24,0}, //Y
   {54,127,127,127,62,28,8,0}, //HEART
+  {120,108,102,102,102,108,120,0}, //D
+  {60,24,24,24,24,24,60,0}, //I
+  {60,102,96,96,96,102,60,0}, //C
+  {60,66,165,129,165,153,66,60} //SML
   
 };
 
 uint8_t msg[] = {
-    //LETTER_G, LETTER_A, LETTER_M, LETTER_E, LETTER_SPC, LETTER_O, LETTER_V, LETTER_E, LETTER_R
-    LETTER_J, LETTER_A, LETTER_S, LETTER_O, LETTER_N, LETTER_HRT, LETTER_K, LETTER_A, LETTER_T, LETTER_H, LETTER_Y, LETTER_SPC
+    CHR_H, CHR_O, CHR_O, CHR_K, CHR_E, CHR_D, CHR_SPC, CHR_O, CHR_N, CHR_SPC, CHR_T, CHR_R, CHR_O, CHR_N, CHR_I, CHR_C, CHR_S, CHR_SPC, CHR_SML, CHR_SPC
   };
 
-uint8_t len_msg = 12;
+uint8_t len_msg = 20;
+int msgindex;
+uint8_t offset;
+uint8_t left_dst[8];
+uint8_t right_dst[8];
+uint8_t temp_result[8];
+uint8_t *result;
+uint8_t *left_src;
+uint8_t *right_src;
 
 void loop (void) {
   // inner loop
   for (uint8_t i = 0; i < len_msg * 8; i++) {
-    int msgindex = i / 8;
-    uint8_t offset = i % 8;
-    uint8_t *result = NULL;
+    msgindex = i / 8;
+    offset = i % 8;
 
     // return the whole character
     if (offset == 0) {
@@ -114,31 +128,21 @@ void loop (void) {
 
     // we are spanning two characters
     else {
-      if (result != NULL)
-        delete result;
-        
-      result = new uint8_t[8];
-      uint8_t *left_dst = new uint8_t[8];
-      uint8_t *right_dst = new uint8_t[8];
-      uint8_t *left_src = letter[msg[msgindex]];
-      uint8_t *right_src = msgindex > (len_msg - 1) ? letter[LETTER_SPC] : letter[msg[msgindex + 1]];
+      left_src = letter[msg[msgindex]];
+      right_src = msgindex > (len_msg - 1) ? letter[CHR_SPC] : letter[msg[msgindex + 1]];
       
       // iterate over array & shift by the offset 
       for (uint8_t j = 0; j < 8; j++) {
         left_dst[j] = left_src[j] << offset;
         right_dst[j] = right_src[j] >> (8 - offset);
-        result[j] = left_dst[j] | right_dst[j];        
-
+        temp_result[j] = left_dst[j] | right_dst[j];        
       }
 
-      delete left_dst;
-      delete right_dst;
+      result = temp_result;
     }
 
-    //Logger::log("idle");
-    
     render(result);
-    delay(80);
+    delay(60);
   }
 
 }
